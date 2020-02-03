84ea40a96f56b90af5fb4af385bb307cb34a0c2f
varunkverma <varunkverma1996@gmail.com>
2020-02-03 22:58:11 +0530

    added backed routes(just structure)

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) =>
   res.json({ msg: "welocme to the contact-keeper API..." })
 );
 
+// Define Routes
+app.use("/api/users", require("./routes/users.route"));
+app.use("/api/auth", require("./routes/auth.route"));
+app.use("/api/contacts", require("./routes/contacts.routes"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(5000, () => console.log(`Server is listening at port: ${PORT}`));
