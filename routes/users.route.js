130871c9338d055718535bf52fe7b3f1396c82c9
varunkverma <varunkverma1996@gmail.com>
2020-02-04 22:56:47 +0530

    modified user registration route to return a jwt token in case of successful registration

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
@@ -46,9 +48,25 @@ router.post(
 
       await user.save();
 
-      res.send({
-        msg: "User Registered Successfully"
-      });
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
     } catch (err) {
       res.status(500).send({ msg: err.message });
     }
