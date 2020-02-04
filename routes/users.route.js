617b8fdce002309f5727591c06c769e93383276c
varunkverma <varunkverma1996@gmail.com>
2020-02-04 22:42:10 +0530

    modified the post user route to register a user in database with validations

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,11 +1,58 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/User");
 
 // @route   GET api/users
 // @desc    Register a user
 // @access  Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please enter a name")
+      .not()
+      .isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password of 6 or more character"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).send({ msg: "User already exists" });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send({
+        msg: "User Registered Successfully"
+      });
+    } catch (err) {
+      res.status(500).send({ msg: err.message });
+    }
+  }
+);
 
 module.exports = router;
