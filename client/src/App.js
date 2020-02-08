3067b10a8268b42e5ab41b969bf796af496c6017
varunkverma <varunkverma1996@gmail.com>
2020-02-08 20:04:08 +0530

    added login component and modified navbar component to provide navigation links for Login and Registration Pages

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Navbar from "./components/layout/navbar.component";
 import Home from "./components/pages/home.component";
 import About from "./components/pages/about.component";
 import Register from "./components/auth/register/register.component";
+import Login from "./components/auth/login/login.component";
 
 const App = () => (
   <AuthState>
@@ -21,6 +22,7 @@ const App = () => (
               <Route exact path="/" component={Home} />
               <Route exact path="/about" component={About} />
               <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
             </Switch>
           </div>
         </div>
---
+++ client/src/components/auth/login/login.component.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  const { email, password } = user;
+
+  const handleOnChange = e =>
+    setUser({ ...user, [e.target.name]: e.target.value });
+
+  const handleOnSubmit = e => {
+    e.preventDefault();
+    console.log("Login successful");
+  };
+
+  return (
+    <div className="form-container">
+      <h1>
+        Account <span className="text-primary">Login</span>
+      </h1>
+      <form onSubmit={handleOnSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleOnChange}
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Login"
+          className="btn btn-primary btn-block"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
