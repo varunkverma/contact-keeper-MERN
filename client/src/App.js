953ca36cae67e0e5689e6f04da7948079ab1ddd1
varunkverma <varunkverma1996@gmail.com>
2020-02-08 14:34:56 +0530

    added context files for managing state of a signed in user

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,27 @@ import "./App.css";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
+
 import Navbar from "./components/layout/navbar.component";
 import Home from "./components/pages/home.component";
 import About from "./components/pages/about.component";
 const App = () => (
-  <ContactState>
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-          </Switch>
+  <AuthState>
+    <ContactState>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
-  </ContactState>
+      </Router>
+    </ContactState>
+  </AuthState>
 );
 
 export default App;
