71f6dadfd0452bd231ba46a34ad9ed33c2ddd2a5
varunkverma <varunkverma1996@gmail.com>
2020-02-09 21:21:23 +0530

    fixing PORT issue

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log(`Server is listening at port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server is listening at port: ${PORT}`));
